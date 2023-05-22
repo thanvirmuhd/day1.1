b9a5fe05ccd59646818bd03649620d8cd170002b
ICT ACADEMY <thanvirmuhd@gmail.com>
2023-05-22 17:04:31 +0530

    first commit

---
+++ day1.1/script.js
@@ -0,0 +1,14 @@
+// program to reverse a string
+
+function reverseString(str) {
+
+    // empty string
+    let newString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        newString += str[i];
+    }
+    return newString;
+}
+
+const result = reverseString("thanvir");
+console.log(result);
